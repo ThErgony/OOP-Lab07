8a1a21882eec2d0546c41bc4fcac887e23a7ac0a
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-11-12 09:23:47 +0100

    Complete exercize anonymous

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,16 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        /**
+         * Use anonymous class for sort incrementally by age
+         * sort list of getFollowedUsers by age of users from min to max
+         */
+        Collections.sort(denzelUsers, new Comparator<User>() {
+        	public int compare(final User o1, final User o2) {
+        		return o1.getAge() - o2.getAge();
+        	}
+		});
+        
         /*
          * expected Result
          */
@@ -105,7 +116,16 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        /**
+         * Use anonymous class for sort incrementally by age
+         * sort list of getFollowedUsers by age of users from max to min
+         */
+        Collections.sort(rossiUsers, new Comparator<User>() {
+        	public int compare(final User o1, final User o2) {
+        		return o2.getAge() - o1.getAge();
+        	}
+		});
+        
         /*
          * expected Result
          */
