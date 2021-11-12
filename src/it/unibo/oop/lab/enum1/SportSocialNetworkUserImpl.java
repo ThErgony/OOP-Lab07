6c22be0d87ad954c96cb2b2995ef19f3f0c31e47
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-11-12 13:17:08 +0100

    uncomment import and method to use it

--- src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
@@ -5,7 +5,7 @@ package it.unibo.oop.lab.enum1;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
+//import java.util.TreeSet;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
